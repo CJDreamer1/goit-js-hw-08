26b35ed68817c44f1fbaf76be2a9d00e9c29d0a1
CJDreamer1 <149508711+CJDreamer1@users.noreply.github.com>
2024-01-13 08:54:11 +0100

    index.html

---
+++ task-1.js
@@ -0,0 +1,5 @@
+const quantity = 10;
+const pricePerDroid = 500;
+const totalPrice =  quantity * pricePerDroid ;
+const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+console.log(makeTransaction(10, 500));
