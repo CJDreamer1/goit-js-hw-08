c154d824ac9bdbc0ae3872b02efd62b4ba688038
CJDreamer1 <149508711+CJDreamer1@users.noreply.github.com>
2024-02-27 09:25:35 +0100

    index.html

--- js/task-5.js
+++ js/task-5.js
@@ -3,12 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const widget = document.querySelector('.widget');
+const body = document.querySelector('body');
 const span = document.querySelector('.color');
 const changeColor = document.querySelector('.change-color');
 
 changeColor.addEventListener('click', function () {
   const randomColor = getRandomHexColor();
-  widget.style.backgroundColor = randomColor;
+  body.style.backgroundColor = randomColor;
   span.textContent = randomColor;
 });
